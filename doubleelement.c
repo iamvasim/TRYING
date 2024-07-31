03711d15814e7fcf01f605e6941e9691352365d1
iamvasim <156228482+iamvasim@users.noreply.github.com>
2024-07-31 12:44:27 +0530

    Add files via upload

---
+++ doubleelement.c
@@ -0,0 +1,8 @@
+/*given an array of integers with 1to n elements and the size of the array
+is n+1. one element is occuring more then once i.e duplicate number is 
+present . find the duplicate element.
+*/
+#include<stdio.h>
+int main(){
+    int arr[]={}
+}
